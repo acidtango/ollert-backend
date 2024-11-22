5f7c36c8a5d0a85fe9da56214ff4f502af78bc2e
Daute Rodríguez <daute@acidtango.com>
2024-11-22 16:05:29 +0000

    remove uneeded stuff

--- functions/addColumn.ts
+++
@@ -1,9 +0,0 @@
-import { GleeFunction } from "@asyncapi/glee"
-
-const addColumn: GleeFunction = async ({ payload, channel }) => {
-    console.log("addColumn", payload, channel)
-
-    return {reply: []}
-}
-
-export default addColumn
